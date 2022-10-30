1850b666c676db35c167faee4e4eaa21ed8c6926
mariasotog <marisogo@myuax.com>
2022-10-30 18:53:25 +0100

    Update vikingClasses.py

--- vikingClasses.py
+++ vikingClasses.py
@@ -19,6 +19,16 @@ class Soldier:
 
 class Viking(Soldier):
     
+    def __init__(self, name, health, strength): 
+        self.name = name
+        super().__init__(health, strength)
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            return "{} has died in act of combat".format(self.name)
+        else:
+            return "{} has received {} points of damage".format(self.name, damage)
 
 # Saxon
 
