716738e4719ec1dc2bfc1efef98b702df625bc21
mariasotog <marisogo@myuax.com>
2022-10-30 18:51:45 +0100

    Update vikingClasses.py

--- vikingClasses.py
+++ vikingClasses.py
@@ -4,17 +4,14 @@
 
 class Soldier:
     
-    def __init__(self, health, strength):
-        
+    def __init__(self, health, strength): 
         self.health = health
         self.strength = strength
     
-    def attack(self):
-        
+    def attack(self):  
         return self.strength
     
     def receiveDamage(self, damage):
-        
         self.health = self.health - damage
 
 # Viking
